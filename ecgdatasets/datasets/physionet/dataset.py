611979832036d32b5debd9412ecc9ee304cd6a22
Rostislav Epifanov <rostepifanov@gmail.com>
2023-10-04 14:23:04 +0700

    add template for physionet

--- ecgdatasets/datasets/physionet/dataset.py
+++ ecgdatasets/datasets/physionet/dataset.py
@@ -1,6 +1,77 @@
+import hashlib
+
+from tqdm import tqdm
+from requests import get
+
+import ecgdatasets.datasets.misc as M
+
 from ecgdatasets.core import EcgDataset
 
 class PhysioNetDataset(EcgDataset):
     """
     Base class for making datasets from PhysioNet source.
     """
+    _template_url = 'https://www.physionet.org/static/published-projects/{}/{}-{}.zip'
+
+    def __init__(
+        self,
+        root,
+        version,
+        download=False,
+        mapper=None,
+        ):
+        """
+        :args:
+            root (string): root directory of dataset.
+            version (string): version of dataset for usage.
+            download (bool):  If true, downloads the dataset from the internet and
+                puts it in root directory. If dataset is already downloaded, it is
+                not downloaded again.
+            mapper(callable or None):   function to transform targets. If None, it
+                is used default mapper.
+        """
+        super(EcgDataset, self).__init__(root, download, mapper)
+        self.version = version
+
+        if download:
+            self.download()
+
+        if not self._check_integrity():
+            raise RuntimeError('Dataset not found. You can use download=True to download it')
+
+        self.data, self.targets = self._load_data()
+
+    @property
+    def _url(self):
+        raise NotImplementedError
+
+    @property
+    def _name(self):
+        raise NotImplementedError
+
+    @property
+    def _zippath(self):
+        return self.root / ( self._name + '.zip' )
+
+    @property
+    def _hash(self):
+        raise NotImplementedError
+
+    def _check_integrity(self):
+        return M._check_integrity(self._zippath, self._hash, M._calculate_sha256)
+
+    def download(self):
+        if self._check_integrity():
+            return
+
+        req = get(self._url, stream=True)
+
+        if req.status_code == 200:
+            with open(self._zippath, 'wb') as f:
+                for chunk in tqdm(req):
+                    f.write(chunk)
+        else:
+            raise RuntimeError('Remote resourse {} is not avialable.'.format(resourse))
+
+    def _load_data(self):
+        raise NotImplementedError
