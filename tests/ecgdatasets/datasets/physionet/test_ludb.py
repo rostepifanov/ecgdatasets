4250c1b207d6df4c91140dd0c9ef7fa0fc87cb16
Rostislav Epifanov <rostepifanov@gmail.com>
2023-10-05 16:20:44 +0700

    fix bug in ludb

--- tests/ecgdatasets/datasets/physionet/test_ludb.py
+++ tests/ecgdatasets/datasets/physionet/test_ludb.py
@@ -17,3 +17,12 @@ def test_LUBD_CASE_download_version_1_0_1():
     datadir = Path(__file__).parent.parent.parent.parent / 'files'
 
     LUBD(datadir, '1.0.1', download=True, mapper=None)
+
+@pytest.mark.skipif(os.getenv('EDBCACHE', None) is None, reason='Set EDBCACHE to run tests.')
+def test_LUBD_CASE_check_shape():
+    datadir = Path(__file__).parent.parent.parent.parent / 'files'
+
+    dataset = LUBD(datadir, '1.0.1', download=False, mapper=None)
+
+    assert dataset[0].shape == (5000, 12)
+    assert dataset[199].shape == (5000, 12)
